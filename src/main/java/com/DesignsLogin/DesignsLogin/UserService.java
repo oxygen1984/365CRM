98328c2196cf96ead37892d07d7dca264af13d70
oxygen1984 <huguettemkipasa@gmail.com>
2021-01-17 00:14:22 -0500

    LoginSystem

---
+++ src/main/java/com/DesignsLogin/DesignsLogin/UserService.java
@@ -0,0 +1,11 @@
+package com.DesignsLogin.DesignsLogin;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+public interface UserService  extends UserDetailsService {
+
+    User findByEmail(String email);
+
+    User save(UserRegistrationDto registration);
+}
+
---
+++ src/test/java/com/DesignsLogin/DesignsLogin/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DesignsLogin.DesignsLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
